a6ad3e3567f4ead2f05522aa88ea0ef006ba41f2
Anuj Anthwal <anujanthwal98765432@gmail.com>
2025-03-25 10:28:20 +0530

    creating project

---
+++ chati/src/main/java/com/app/chati/ChatiApplication.java
@@ -0,0 +1,13 @@
+package com.app.chati;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatiApplication.class, args);
+	}
+
+}
---
+++ chati/src/test/java/com/app/chati/ChatiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.chati;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
